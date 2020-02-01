546f82c6f3d8092b918ff125e069d9780ad471de
Rebecca <rebeccabushko@gmail.com>
2020-02-01 11:33:26 -0500

    WIP Input Manager

---
+++ GGJEmoji/EmojiHell/Assets/Scripts/InputManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public InputField input;
+    private List<string> selections;
+
+    public void NewInput(List<string> selections)
+    {
+        // input.
+        this.selections = selections;
+    }
+
+    
+}
