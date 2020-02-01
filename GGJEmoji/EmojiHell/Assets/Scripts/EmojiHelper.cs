d3acf0c9f7e1669b4010d991045b88e8f80b1607
Rebecca <rebeccabushko@gmail.com>
2020-02-01 13:54:21 -0500

    Emoji helper work

---
+++ GGJEmoji/EmojiHell/Assets/Scripts/EmojiHelper.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmojiHelper : MonoBehaviour
+{
+    private EmojiType type;
+    private List<string> selections;
+
+    public SpriteRenderer sprite;
+
+    public void Init(EmojiType type, List<string> selections)
+    {
+        this.type = type;
+        this.selections = selections;
+
+        SetSprite();
+    }
+
+    private void SetSprite()
+    {
+        switch(type)
+        {
+            case EmojiType.GOOD:
+                // Set good emoji sprite
+                return;
+            case EmojiType.BAD:
+                // Bad emoji sprite
+                return;
+            default:
+                Debug.LogError("Emoji type was not good or bad");
+                break;
+        }
+    }
+}
