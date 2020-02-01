81f9769a3ce74eee3df9e6b14bca41a22c6f6c7e
Rebecca <rebeccabushko@gmail.com>
2020-02-01 13:21:01 -0500

    Basic input validation finished

--- GGJEmoji/EmojiHell/Assets/Scripts/InputManager.cs
+++ GGJEmoji/EmojiHell/Assets/Scripts/InputManager.cs
@@ -9,9 +9,14 @@ public class InputManager : MonoBehaviour
     private List<string> selections;
     private string currentSelection = string.Empty;
 
-    public void Init()
+    void Start()
     {
-        // input.onValueChanged += ValidateText;
+        // Temp starting selections
+        selections = new List<string>();
+        selections.Add("apple");
+        selections.Add("bee");
+        selections.Add("candle");
+        selections.Add("dog");
     }
 
     public void NewInput(List<string> selections)
@@ -26,28 +31,42 @@ public class InputManager : MonoBehaviour
 
     public void ValidateText(string newString)
     {
+        Debug.LogError("Current selection is " + currentSelection);
+        Debug.LogError("New string is " + newString);
         // Player hasn't started typing a valid word
         if (currentSelection.Equals(string.Empty))
         {
             // Find the word they are trying to type
             foreach (string selection in selections)
             {
-                if (selection.IndexOf(input.text) >= 0)
+                if (selection.IndexOf(newString) == 0)
                 {
+                    // If we have found the word they're typing,
+                    // go ahead and return
                     currentSelection = selection;
                     return;
                 }
+
+                // If a word was not set, reset the text field
+                input.text = string.Empty;
+                currentSelection = string.Empty;
             }
         }
         // Validate that they are continuing to type the correct word
         else
         {
-            if (currentSelection.IndexOf(input.text) == -1)
+            if (currentSelection.IndexOf(newString) == -1)
             {
                 // Remove the last character they typed
-                string currentText = input.text;
-                input.text = currentText.Substring(0, currentText.Length - 1);
+                input.text = newString.Substring(0, newString.Length - 1);
+                Debug.LogError("Input is " + input.text);
             }
         }
+    }
+
+    public void OnSubmit(string finalString)
+    {
+        // Submit the score
+        NewInput(selections);
     }
 }
