99c35d4ec39a7799c0aea985d03422d8a3cb4180
Rebecca <rebeccabushko@gmail.com>
2020-02-01 21:39:36 -0500

    i hope this compiles

--- GGJEmoji/EmojiHell/Assets/Scripts/AssetManager.cs
+++ GGJEmoji/EmojiHell/Assets/Scripts/AssetManager.cs
@@ -6,6 +6,9 @@ public class AssetManager : MonoBehaviour
 {
     private static Dictionary<int, Sprite> emojis = new Dictionary<int, Sprite>();
 
+    private static List<string> goodList = new List<string>();
+    private static List<string> badList = new List<string>();
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +18,12 @@ public class AssetManager : MonoBehaviour
             int emojiNum = int.Parse(s.name.Substring(s.name.LastIndexOf("_") + 1));
             emojis.Add(emojiNum, s);
         }
+
+        TextAsset tempList = Resources.LoadAll<TextAsset>("Lists/goodlist.txt");
+        goodList = sourceTexts.text.Split("\n"[0]);
+
+        tempList = Resources.LoadAll<TextAsset>("Lists/badlist.txt");
+        badList = sourceTexts.text.Split("\n"[0]);
     }
 
     public static Sprite GetEmoji(int emojiNum)
